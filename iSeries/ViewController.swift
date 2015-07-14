790be1a41b0e120b7ba66bd65e4e60bd2cf0aa54
Oliver Rosner <Schilum23@olivers-macbook-pro.local>
2015-07-14 19:05:02 +0200

    Initial Commit

---
+++ iSeries/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSeries
+//
+//  Created by Oliver Rosner on 14.07.15.
+//  Copyright © 2015 Oliver Rosner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
